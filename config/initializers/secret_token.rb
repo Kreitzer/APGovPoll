74b001606e8d9f62485eb3c703e2d7012b7ecb20
Dylan <kreitzer13383d@gmail.com>
2012-11-04 21:27:24 -0500

    Created Poll Response

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+APGovPoll::Application.config.secret_token = 'cf18307f4cf026221e8e848a7266dfed1173f8b0c87587122e4202cd8d9f1da727535e856500213e7e99b6770194826b230bb7bea9f8b980083ec1ec2c3fc792'
